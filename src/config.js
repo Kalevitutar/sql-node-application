04d93f634dcee15711c15d802bcb394f16674d81
Kalevitutar <tanyamthomas@yahoo.com>
2023-12-19 15:24:52 -0500

    problems with create

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const config = {
+  user: "sql_postgres_practice_2p60_user",
+  host: "dpg-cm086f0cmk4c7380jvf0-a.oregon-postgres.render.com",
+  database: "sql_postgres_practice_2p60",
+  password: "VjKSryAfsbEPTcUoow840JL4vI9w6dae",
+  port: 5432,
+  ssl: true,
+};
+module.exports = config;
---
+++ src/index.js
@@ -0,0 +1,79 @@
+const express = require("express"); //external module for using express
+const { Client } = require("pg"); //external module for using postgres with node
+const config = require("./config.js"); // internal module for connecting to our config file
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const client = new Client(config); //creating our database Client with our config values
+
+//HELPER FUNCTIONS
+
+const addLanguage = async (
+  name,
+  releasedYear,
+  githutRank,
+  pyplRank,
+  tiobeRank
+) => {
+  await client.connect();
+  const superego = 2000;
+  console.log("this is the id ", superego);
+  const createdAt = Date.now();
+  const updatedAt = Date.now();
+  const result = await client.query(
+    `INSERT INTO programming_languages (id, name, released_year, githut_rank, pypl_rank, tiobe_rank, created_at, updated_at) VALUES (${superego}, ${name}, ${releasedYear}, ${githutRank}, ${pyplRank}, ${tiobeRank}, ${createdAt}, ${updatedAt})`
+  );
+  console.log(result.rows);
+  return result.rows;
+};
+
+const getLanguages = async () => {
+  await client.connect(); //connecting to our database
+  const result = await client.query("SELECT * FROM programming_languages"); //effectively saying "hit the play button - do the query"
+  console.log(result.rows); //will always have a keycode called "rows" that contains the data
+  await client.end(); //ending the connection to our database
+  return result.rows;
+};
+
+const getLanguage = async (id) => {
+  await client.connect(); //connecting to our database
+  const result = await client.query(
+    `SELECT * FROM programming_languages WHERE id = ${id}`
+  );
+  console.log(result.rows);
+  await client.end(); //ending the connection to our database
+  return result.rows;
+};
+
+//ROUTES
+
+app.post("/add-language", async (req, res) => {
+  const name = req.body.name;
+  const releasedYear = req.body.released_year;
+  const githutRank = req.body.githut_rank;
+  const pyplRank = req.body.pypl_rank;
+  const tiobeRank = req.body.tiobe_rank;
+  await addLanguage(name, releasedYear, githutRank, pyplRank, tiobeRank);
+  res
+    .status(201)
+    .json(
+      "Congratulations on creating a language! I hope you have at least ten years' experience in it"
+    );
+});
+
+app.get("/get-languages", async (req, res) => {
+  const languages = await getLanguages();
+  res.send(languages);
+});
+
+app.get("/get-language/:id", async (req, res) => {
+  const language = await getLanguage(req.params.id);
+  res.send(language);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
